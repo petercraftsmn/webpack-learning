c272a15954446976324e8d2e42ebb393de29c82f
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2020-04-21 03:28:35 -0400

    Asset management finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import './style.css';
+import City from './city.jpg';
+
 function component() {
     const element = document.createElement( 'div' );
     element.innerHTML = [ 'Hello ' + 'world' ].join();
+    element.classList.add( 'hello' );
+
+    const myImage = new Image();
+    myImage.src = City;
+    element.appendChild( myImage );
+
     return element;
 }
 
