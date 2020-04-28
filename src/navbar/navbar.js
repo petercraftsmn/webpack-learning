8ac4b0de2da22b4cd1e03a3d7a2aab26daba89f0
Claudia <claudia.pankaj@gmail.com>
2020-04-28 10:41:30 -0400

    navBar function creat

---
+++ src/navbar/navbar.js
@@ -0,0 +1,31 @@
+import "./navbar-style.css";
+import menuIconSvg from "./menu-24px.svg";
+import personIconSvg from "./person_outline-24px.svg";
+
+export default function navBar() {
+
+    let navElement;
+    navElement = document.createElement('nav');
+    document.body.appendChild(navElement);
+    navElement.classList.add("nav-element");
+    navElement.classList.add("shadow");
+
+    let menuIcon;
+    menuIcon = document.createElement('img');
+    navElement.appendChild(menuIcon);
+    menuIcon.src = menuIconSvg;
+    menuIcon.classList.add('menu-icon');
+
+    let profileIcon;
+    profileIcon = document.createElement('img');
+    navElement.appendChild(profileIcon);
+    profileIcon.src = personIconSvg;
+    profileIcon.classList.add("profile-icon");
+
+    let logo;
+    logo = document.createElement('h2');
+    navElement.appendChild(logo);
+    logo.innerHTML = "Claudia";
+    logo.classList.add("logo");
+}
+
