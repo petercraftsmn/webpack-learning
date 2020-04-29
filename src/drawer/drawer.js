6784b091934e1e7772fcb50a5bea80414c837627
Claudia <claudia.pankaj@gmail.com>
2020-04-29 13:19:35 -0400

    media query added

--- src/drawer/drawer.js
+++ src/drawer/drawer.js
@@ -1,30 +1,42 @@
+'use strict';
 import "./drawer-style.css";
 
 export default class Drawer {
     constructor() {
-        let drawerWrapper = document.createElement('div');
-        document.body.appendChild(drawerWrapper);
-        drawerWrapper.classList.add("drawer-wrapper");
-        drawerWrapper.classList.add("drawer-invisible");
+        // Create overlay for drawer
+        let drawerOverlay = document.createElement('div');
+        document.body.appendChild(drawerOverlay);
+        drawerOverlay.classList.add("drawer-overlay");
+        drawerOverlay.classList.add("drawer-invisible");
 
+        // Create drawer
         let drawerMain;
         drawerMain = document.createElement('div');
-        drawerWrapper.appendChild(drawerMain);
+        drawerOverlay.appendChild(drawerMain);
         drawerMain.classList.add('drawer-main');
 
         // Open drawer
         document.querySelector('.menu-icon').onclick = function (event) {
             event.stopPropagation();
             event.preventDefault();
-            drawerWrapper.classList.remove('drawer-invisible');
+            drawerOverlay.classList.remove('drawer-invisible');
         }
 
         // Close drawer
-        drawerWrapper.onclick = function (event) {
+        drawerOverlay.onclick = function (event) {
             event.stopPropagation();
             event.preventDefault();
             if (event.target !== this) return;
-            drawerWrapper.classList.add('drawer-invisible');
+            drawerOverlay.classList.add('drawer-invisible');
         }
+
+        // Add logo to drawer
+        let logo;
+        logo = document.createElement('div');
+        drawerMain.appendChild(logo);
+        logo.classList.add("logo");
+        logo.innerHTML = "Claudia";
+
+
     }
 }
