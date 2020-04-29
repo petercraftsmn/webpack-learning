6297f493188995e6dc7968de0826d543aa89adaa
Claudia <claudia.pankaj@gmail.com>
2020-04-29 02:43:53 -0400

    drawer is working

--- src/drawer/drawer.js
+++ src/drawer/drawer.js
@@ -5,17 +5,26 @@ export default class Drawer {
         let drawerWrapper = document.createElement('div');
         document.body.appendChild(drawerWrapper);
         drawerWrapper.classList.add("drawer-wrapper");
-        drawerWrapper.onclick = function (event) {
-            event.stopPropagation();
-            event.preventDefault();
-            if (event.target !== this) return;
-            drawerWrapper.classList.add('drawer-invisible');
-        }
+        drawerWrapper.classList.add("drawer-invisible");
 
         let drawerMain;
         drawerMain = document.createElement('div');
         drawerWrapper.appendChild(drawerMain);
         drawerMain.classList.add('drawer-main');
 
+        // Open drawer
+        document.querySelector('.menu-icon').onclick = function (event) {
+            event.stopPropagation();
+            event.preventDefault();
+            drawerWrapper.classList.remove('drawer-invisible');
+        }
+
+        // Close drawer
+        drawerWrapper.onclick = function (event) {
+            event.stopPropagation();
+            event.preventDefault();
+            if (event.target !== this) return;
+            drawerWrapper.classList.add('drawer-invisible');
+        }
     }
 }
